80480feec5b27cb3ba424fdbfe760b2691a35b64
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-07-06 13:53:59 +0100

    wrote Hello, World! Zig server

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,23 @@
 const std = @import("std");
+const zap = @import("zap");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+fn on_request(r: zap.Request) void {
+    r.sendBody("<h1>Hello, Zig!</h1>") catch return;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    var listener = zap.HttpListener.init(.{
+        .port = 3000,
+        .on_request = on_request,
+        .log = true,
+        .max_clients = 100000,
+    });
+    try listener.listen();
+
+    std.debug.print("Starting server on 0.0.0.0:3000\n", .{});
+
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
 }
